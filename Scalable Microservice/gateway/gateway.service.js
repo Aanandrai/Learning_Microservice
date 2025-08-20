d153cc8c9f49f95e531d89bb2b6f67ad0a0850c3
Aanand Rai <aanand.rai@gigaforce.io>
2025-08-20 12:56:48 +0530

    learning_phase3

---
+++ Scalable Microservice/gateway/gateway.service.js
@@ -0,0 +1,21 @@
+import express from "express"
+import proxy from "express-http-proxy"
+
+const app=express()
+
+// This is running on this local system 
+app.use("/stress-test", proxy('http://localhost:3001'))
+
+
+// This route is running on another machine 
+// we can access it by port forwarding
+//for help refer to youtube, VS port forwording
+// This is running on other Machine so "xyz" is port forwarding url 
+app.use("/", proxy('xyz'))
+
+
+app.listen(3000,()=>{
+    console.log(`app is listen on post ${3000}`)
+})
+
+// app.use("/",proxy())
---
+++ Scalable Microservice/server1/server1.js
@@ -0,0 +1,32 @@
+import express from "express";
+import morgan from "morgan";
+import os from "os";
+import cluster from "cluster";
+
+const numCPUs = os.cpus().length;
+
+if (cluster.isPrimary) {
+    console.log(`Primary process ${process.pid} is running`);
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died. Restarting...`);
+        cluster.fork();
+    });
+} else {
+    const app = express();
+    app.use(morgan("dev"));
+
+    app.get("/stress-test", (req, res) => {
+        for (let i = 0; i < 1000000000; i++) {
+            // Simulate heavy computation
+        }
+        res.send("World is listening");
+    });
+
+    app.listen(3001, () => {
+        console.log(`Worker ${process.pid} running on http://localhost:3001`);
+    })
+}
